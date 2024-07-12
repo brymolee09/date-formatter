5ba5d21fc941d94dd17f78d5c29e8988f9a4d1cf
brymolee09 <110599834+brymolee09@users.noreply.github.com>
2024-07-12 17:49:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const currentDateParagraph = document.getElementById('current-date')
+const dateOptionsSelectElement = document.getElementById('date-options')
+
+const date= new Date()
+const day = date.getDate()
+
+const month = date.getMonth() + 1
+
+const year = date.getFullYear()
+
+const hours = date.getHours()
+
+const minutes = date.getMinutes()
+
+const formattedDate = `${day}-${month}-${year}`
+console.log(formattedDate)
+currentDateParagraph.textContent = formattedDate
+
+dateOptionsSelectElement.addEventListener('change', () => {
+  switch(dateOptionsSelectElement.value){
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-")
+      break
+      case "mm-dd-yyyy-h-mm":
+        currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`
+        break
+        case "dd-mm-yyyy-h-mm":
+          currentDateParagraph.textContent = `${day}-${month}-${year} ${hours} Hours ${minutes} Minutes`
+          break
+        default:
+        currentDateParagraph.textContent = formattedDate
+  }
+})
+/*const greet = 'ygolonhcet'
+const newGreet= greet.split("").reverse().join("")
+console.log(newGreet)*/
